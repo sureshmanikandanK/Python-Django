e97c5003dafdf30335a3e9253f1537b0fbb686b4
Sureshmanikandan K <sureshmanikandan.k>
2024-08-20 14:40:49 +0530

    Update

--- forms/user_profile/forms.py
+++ forms/user_profile/forms.py
@@ -1,4 +1,5 @@
 from django import forms
  
 class forms_page_class(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label= 'Enter Your Name : ',max_length=20,required=False,
+        error_messages={'required': 'Please enter your name.'})
--- forms/user_profile/views.py
+++ forms/user_profile/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import forms_page_class
 
-
-# Create your views here.
 def forms_page(request):
     if request.method == 'POST':
-        enter_username = request.POST['username']
-        print(enter_username)
-        if enter_username == "":
-            return render(request,'user_profile/forms.html',{
-                'haserror':True
-            })
-        return HttpResponseRedirect('/user/Thanks')
-    form = forms_page_class()
-    return render(request,'user_profile/forms.html',{'formss':form})
+        form = forms_page_class(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/user/Thanks')
+    #     else:
+    #         return render(request, 'user_profile/forms.html', {
+    #             'formss': form,
+    #             'formss.user_name.errors':True
+    #         })
+    # else:
+        form = forms_page_class()
+
+        return render(request, 'user_profile/forms.html', {'formss': form})
+
 def Thanks(request):
-    return render(request,'user_profile/thank_you.html')
-
-
+    return render(request, 'user_profile/thank_you.html')
