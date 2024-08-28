82fe7f18e9fc45bc82a6055afb7b84540277db70
Sureshmanikandan K <sureshmanikandan.k>
2024-08-28 10:13:40 +0530

    update

--- bookstore/book_app/models.py
+++ bookstore/book_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
+from author_app.models import Author
+
 
 # Create your models here.
+class bookmodel(models.Model):
+    title = models.CharField(max_length=255)
+    rating = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),MinValueValidator(1)
+        ]
+    )
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        
